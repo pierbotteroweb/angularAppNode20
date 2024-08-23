290f74271808e70a7d7ef1857f5dcc97892ba881
pierbotteroweb <pierbotteroweb@gmail.com>
2024-08-23 19:47:58 -0300

    project-renamed

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -16,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'angularAppNode20'`, () => {
+  it(`should have as title 'isadora-sene'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('angularAppNode20');
+    expect(app.title).toEqual('isadora-sene');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('angularAppNode20 app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('isadora-sene app is running!');
   });
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angularAppNode20';
+  title = 'isadora-sene';
 }
